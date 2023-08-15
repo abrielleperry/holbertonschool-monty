bfda9ab40790063ca23bb95fe7ed2569793143d9
abrielleperry <abrielleperry22@icloud.com>
2023-08-14 19:11:25 -0700

    betty pall

--- _pint.c
+++ _pint.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * _pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number where the instruction appears.
+ */
+
 void _pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
