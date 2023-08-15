fee5db957e8012012b61c56c1981ef9b7cb1371a
abrielleperry <abrielleperry22@icloud.com>
2023-08-14 19:05:08 -0700

    betty pall

--- _pall.c
+++ _pall.c
@@ -3,9 +3,8 @@
 void _pall(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;  /*line_num unused for pall -silences compiler warning*/
-
 	stack_t *temp = *stack;
-        
+
 	while (temp && temp->next)
 	{
 		temp = temp->next;
