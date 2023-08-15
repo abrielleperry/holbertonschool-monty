d56b132d87763368c53af28adb5f7f4e512cc15e
abrielleperry <abrielleperry22@icloud.com>
2023-08-14 19:29:36 -0700

    betty add

--- _add.c
+++ _add.c
@@ -1,5 +1,14 @@
 #include "monty.h"
 
+/**
+ * _add - Adds the top two elements of the stack.
+ * @stack: A double pointer to the top of the stack.
+ * @line_number: The line number where the add opcode appears.
+ * Description:
+ * If the stack contains less than two elements, prints an error message
+ * with the line number where the error occurred and exits the program
+ * with the status EXIT_FAILURE.
+ */
 
 void _add(stack_t **stack, unsigned int line_number)
 {
