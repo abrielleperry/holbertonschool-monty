83191c5410c16a566ca4fc906115af238eeb5925
abrielleperry <abrielleperry22@icloud.com>
2023-08-14 21:08:51 -0700

    3.m

--- _pall.c
+++ _pall.c
@@ -13,11 +13,6 @@ void _pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;  /*line_num unused for pall -silences compiler warning*/
 	stack_t *temp = *stack;
 
-	while (temp && temp->next)
-	{
-		temp = temp->next;
-	}
-
 	while (temp)
 	{
 		printf("%d\n", temp->n);
